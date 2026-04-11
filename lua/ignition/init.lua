8b6de6d5eb13ee7cb4fc0f5e716ac2c42798069b
Angel Talero <angelgotalero@outlook.com>
2026-04-11 00:34:08 -0500

    Initial commit

---
+++ lua/ignition/init.lua
@@ -0,0 +1,22 @@
+---@class ignition
+local M = {}
+
+--- Configure and initialize the plugin
+--- @param opts ignition.opts
+---     Plugin configuration, will be merged with defaults
+function M.setup(opts)
+end
+
+--- Create a new task
+--- @param task ignition.task Task definition
+function M.task(task)
+
+end
+
+--- Create a set of new tasks
+--- @param tasks table<ignition.task> List of tasks to add,
+function M.tasks(tasks)
+
+end
+
+return M
---
+++ lua/ignition/types.lua
@@ -0,0 +1,7 @@
+--- @meta
+
+--- @class ignition.task
+--- @field desc string Task description
+
+--- @class ignition.opts
+--- @field tasks table?
